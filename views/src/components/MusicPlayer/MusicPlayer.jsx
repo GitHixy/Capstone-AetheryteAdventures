dce154983296d7d16fad1c7fe3ff59c1db125583
Michael <michael.pecora.92@gmail.com>
2024-04-29 14:50:43 +0200

    Tested and working

--- views/src/components/MusicPlayer/MusicPlayer.jsx
+++ views/src/components/MusicPlayer/MusicPlayer.jsx
@@ -1,103 +1,112 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import useSound from "use-sound";
+import { TbPlayerPlayFilled, TbPlayerPauseFilled, TbPlayerTrackNextFilled, TbPlayerTrackPrevFilled } from "react-icons/tb";
+import { IconContext } from 'react-icons/lib';
+import song1 from "../../assets/Beyond The Final Days.mp3"
 import styles from './MusicPlayer.module.css'; 
-import Song1 from '../../assets/Beyond The Final Days.mp3';
-import Song2 from '../../assets/Echoes of Dawn.mp3'
-import Song3 from '../../assets/Hymn of the Rejoining.mp3'
 
-const MusicPlayer = () => {
-    const songs = [
-        { title: "Beyond The Final Days", src: Song1 },
-        { title: "Echoes of Dawn", src: Song2 },
-        { title: "Hymn of the Rejoining", src: Song3 }
-    ];
 
-    const [currentSongIndex, setCurrentSongIndex] = useState(0);
+const MusicPlayer = (coverImg, songTitle, songArtist) => {
     const [isPlaying, setIsPlaying] = useState(false);
-    const [volume, setVolume] = useState(0.50);
-    const [isOpen, setIsOpen] = useState(false);
-    const audioRef = useRef(null);
+    const [play, {pause, duration, sound}] = useSound(song1)
+    const [currTime, setCurrTime] = useState({
+        min: "",
+        sec: "",
+      });
+    const [seconds, setSeconds] = useState();
+    const sec = duration / 1000;
+        const min = Math.floor(sec / 60);
+        const secRemain = Math.floor(sec % 60);
+        const time = {
+          min: min,
+          sec: secRemain
+        };
 
-    useEffect(() => {
-        if (audioRef.current) {
-            audioRef.current.volume = volume;
-        }
-    }, [volume]);
-
-    useEffect(() => {
-        if (isPlaying) {
-            audioRef.current.play();
-        }
-    }, [currentSongIndex, isPlaying]);
-
-    const handlePlayPause = () => {
-        if (isPlaying) {
-            audioRef.current.pause();
-        } else {
-            audioRef.current.play();
-        }
-        setIsPlaying(!isPlaying);
-    };
-
-    const handleStop = () => {
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0;
+    const playingButton = () => {
+    if (isPlaying) {
+        pause();
         setIsPlaying(false);
+    } else {
+        play();
+        setIsPlaying(true);
+    }
     };
 
-    const handlePrevSong = () => {
-        if (currentSongIndex > 0) {
-            setCurrentSongIndex(currentSongIndex - 1);
-        } else {
-            setCurrentSongIndex(songs.length - 1);
-        }
-    };
-
-    const handleNextSong = () => {
-        if (currentSongIndex < songs.length - 1) {
-            setCurrentSongIndex(currentSongIndex + 1);
-        } else {
-            setCurrentSongIndex(0);
-        }
-    };
-
-    const handleVolumeChange = (event) => {
-        setVolume(event.target.value);
-    };
-
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
-
+    useEffect(() => {
+        const interval = setInterval(() => {
+          if (sound) {
+            setSeconds(sound.seek([])); 
+            const min = Math.floor(sound.seek([]) / 60);
+            const sec = Math.floor(sound.seek([]) % 60);
+            setCurrTime({
+              min,
+              sec,
+            });
+          }
+        }, 1000);
+        return () => clearInterval(interval);
+      }, [sound]);
+    
     return (
-        <div className={styles.container}>
-            <button onClick={toggleDropdown} className={styles.dropdownToggle}>
-                Music Player
-            </button>
-            {isOpen && (
-                <div className={styles.dropdownContent}>
-                    <div className={styles.title}>Now Playing: {songs[currentSongIndex].title}</div>
-                    <audio ref={audioRef} src={songs[currentSongIndex].src} preload="auto" />
-                    <div className={styles.controls}>
-                        <button className={styles.button} onClick={handlePlayPause}>
-                            {isPlaying ? 'Pause' : 'Play'}
-                        </button>
-                        <button className={styles.button} onClick={handleStop}>Stop</button>
-                        <button className={styles.button} onClick={handlePrevSong}>Prev</button>
-                        <button className={styles.button} onClick={handleNextSong}>Next</button>
-                        <input
-                            type="range"
-                            min="0"
-                            max="1"
-                            step="0.01"
-                            value={volume}
-                            onChange={e => setVolume(e.target.value)}
-                            className={styles.volumeSlider}
-                        />
-                    </div>
-                </div>
-            )}
+        <div className={styles.component}>
+      <h2>Playing Now</h2>
+      <img
+        className={styles.musicCover}
+        src={coverImg}
+      />
+      <div>
+        <h3 className={styles.title}>{songTitle}</h3>
+        <p className={styles.subtitle}>{songArtist}</p>
+      </div>
+      <div className={styles.timeContainer}>
+        <div className={styles.time}>
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
         </div>
-    );
+        <input
+          type="range"
+          min="0"
+          max={duration / 1000}
+          default="0"
+          value={seconds}
+          className={styles.timeline}
+          onChange={(e) => {
+            sound.seek([e.target.value]);
+          }}
+        />
+      </div>
+      <div className={styles.btnContainer}>
+        <button className={styles.playButton}>
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+          <TbPlayerTrackPrevFilled />
+          </IconContext.Provider>
+        </button>
+        {!isPlaying ? (
+          <button className={styles.playButton} onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <TbPlayerPlayFilled />
+            </IconContext.Provider>
+          </button>
+        ) : (
+          <button className={styles.playButton} onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <TbPlayerPauseFilled />
+            </IconContext.Provider>
+          </button>
+        )}
+        <button className={styles.playButton}>
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+          <TbPlayerTrackNextFilled />
+          </IconContext.Provider>
+        </button>
+      </div>
+    </div>
+        
+    )
 };
 
 export default MusicPlayer;
